4f507e8c6a75ba6645eb4b889b2ba1977ef58949
muhammadsaif1 <saif.shabir12@gmail.com>
2025-10-21 13:50:44 +0500

    feat/pitch-store-setup

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import episodeReducer from "../slices/episodeSlice";
 import messageReducer from "../slices/messageSlice";
 import authReducer from "../slices/authSlice";
+import pitchReducer from "../slices/pitchSlice";
 
 const store = configureStore({
   reducer: {
     episodes: episodeReducer,
     messages: messageReducer,
-    auth: authReducer
+    auth: authReducer,
+    pitches: pitchReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
