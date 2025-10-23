d4411efe68e7759d175d7435a4a3abb76f742af4
muhammadsaif1 <saif.shabir12@gmail.com>
2025-10-23 17:14:32 +0500

    ..

--- src/pages/latest-episodes/EpisodesPage.jsx
+++ src/pages/latest-episodes/EpisodesPage.jsx
@@ -2,17 +2,24 @@ import PageHero from "@/components/PageHero/PageHero";
 import Episodes from "./Episodes";
 import CallToAction from "@/components/Shared/CallToAction";
 import EpisodesList from "@/components/EpisodesCard/Episode-list";
+import "./stle.scss";
 
 const EpisodesPage = () => {
   return (
     <div className="episode-page texture-bg-2">
-      <PageHero
+      {/* <PageHero
         pageTitle="Podcast"
         highlightWord="Episodes"
         highlightColor="tcp-1"
         pb={"pb-lg-15"}
-      />
+      /> */}
       {/* <Episodes /> */}
+      <div className="podcast-heading">
+        <h1>
+          Podcast <span>Episodes</span>{" "}
+        </h1>
+      </div>
+
       <EpisodesList />
       <CallToAction />
     </div>
