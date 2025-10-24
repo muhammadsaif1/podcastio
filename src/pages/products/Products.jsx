e4461da9d6d6b7b2f2689e12e6bdabc5f8e6004e
muhammadsaif1 <saif.shabir12@gmail.com>
2025-10-25 02:07:58 +0500

    fix

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts, clearError } from "@/redux/slices/productSlice";
-import "./ProductsPage.scss";
+import "./productsPage.scss";
 
 const ProductsPage = () => {
   const dispatch = useDispatch();
